58ae7dbf602f324adbed0459132b7aa49994e43e
Anton Silantev <silantev.anton.sergeevich@gmail.com>
2023-02-23 23:39:20 +0400

    Выполнение ДЗ 3.3

---
+++ ДЗ 3.3/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задача 23: Напишите программу, которая принимает 
+//на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[number];
+
+void FillArray(int[] arr)
+{
+    int len = arr.Length;
+    int i = 0;
+    while (i<len)
+    {
+        arr[i] = i + 1;
+        i++;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int len = arr.Length;
+    int i = 0;
+    while (i<len)
+    {
+        Console.WriteLine(arr[i]);
+        i++;
+    }
+}
+
+void CubeArray(int[] arr)
+{
+    int len = arr.Length;
+    int i = 0;
+    while (i<len)
+    {
+        arr[i] = arr[i] * arr[i] * arr[i];
+        i++;
+    }
+}
+
+FillArray(array);
+//PrintArray(array);
+CubeArray(array);
+PrintArray(array);
